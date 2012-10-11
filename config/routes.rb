89342a9ee4922c24e2ed7ac4267515b1ef677b35
Benuuu <user@server>
2012-10-10 21:38:23 -0400

    basic users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Mano::Application.routes.draw do
+  resources :user_sessions
+
+  match 'login' => "user_sessions#new",      :as => :login
+  match 'logout' => "user_sessions#destroy", :as => :logout
+
+  resources :users  # give us our some normal resource routes for users
+  resource :user, :as => 'account'  # a convenience route
+
+  match 'signup' => 'users#new', :as => :signup
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
