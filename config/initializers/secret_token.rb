1d01086ce6a4be70a768354849866f17a176fd02
Benuuu <user@server>
2012-10-10 20:57:59 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mano::Application.config.secret_token = 'e2b12b33f5d38109d7d4c574b630f75caf4117e50874185a44dc4117d7e34ac0b728967395b1f6619de3c1e21476bc90e4d80c603830a3bcf94862b82c55dfaa'
