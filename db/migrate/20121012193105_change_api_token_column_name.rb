1bf13809b920527d9fe0d23d436ea118bb6b6f6d
Joe Burgess <joemburgess@gmail.com>
2013-03-15 14:35:43 -0400

    Fixing the api_tokens table

--- db/migrate/20121012193105_change_api_token_column_name.rb
+++
@@ -1,9 +0,0 @@
-class ChangeApiTokenColumnName < ActiveRecord::Migration
-  def up
-    rename_column :api_tokens, :type, :api_type
-  end
-
-  def down
-    rename_column :api_tokens, :api_type, :type
-  end
-end
--- db/migrate/20130313192152_drop_api_tokens.rb
+++
@@ -1,9 +0,0 @@
-class DropApiTokens < ActiveRecord::Migration
-  def up
-    drop_table :api_tokens
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
