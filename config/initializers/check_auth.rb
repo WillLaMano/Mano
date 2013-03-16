08fd7c79ba200e2ee17798f44455f5794586a50a
Joe Burgess <joemburgess@gmail.com>
2013-03-16 11:16:01 -0400

    Fixing the check_auth to be actually correct

--- config/initializers/check_auth.rb
+++ config/initializers/check_auth.rb
@@ -1,7 +1,7 @@
-puts "#"*30
-puts ""*5
-if !Rails.application.config.auth[:google].empty?
+if Rails.application.config.auth[:google].empty?
+  puts "#"*30
+  puts ""*5
   puts "\e[31mYou don't have google auth codes! get them at https://code.google.com/apis/console/\e[0m"
+  puts ""*5
+  puts "#"*30
 end
-puts ""*5
-puts "#"*30
