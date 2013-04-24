05a41d0d136d1092fffc6f25724c6bac0964aace
Benuuu <ben@benuuu.com>
2013-04-24 17:52:08 -0400

    basic authlogic unit test

--- test/functional/groups_controller_test.rb
+++ test/functional/groups_controller_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class GroupsControllerTest < ActionController::TestCase
+  
+  setup :activate_authlogic
+  
+  setup do
+    @group = FactoryGirl.create(:group_with_users)
+    @user = @group.users.first
+    UserSession.create(@user)
+  end
+  
   test "should get view" do
-    get :view
+    get :show, :id => @group.id
     assert_response :success
   end
 
