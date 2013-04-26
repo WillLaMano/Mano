077b6c10babcd7e82f14131451891df3d2d579d7
Joe Burgess <joemburgess@gmail.com>
2013-04-26 14:27:24 -0400

    Fixed Google Refresh Token BS

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,7 +1,7 @@
 class Authorization < ActiveRecord::Base
   attr_accessible :auth_token, :auth_type, :user_id, :refresh_token, :expires_at
   attr_accessor :params
-  validate :check_access_token
+  before_create :check_access_token
   belongs_to :user
 
   def self.services
