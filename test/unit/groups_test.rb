7fb557fa78ed6131cc6561d72e2a70fddbe1a067
Benuuu <benwu00@gmail.com>
2013-05-14 17:18:46 -0400

    group unit test. changed group controller test to be more of a functional test

--- test/unit/groups_test.rb
+++ test/unit/groups_test.rb
@@ -4,4 +4,17 @@ class GroupTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+  
+  test "name needs to be present" do
+    group = FactoryGirl.build(:group)
+    group.name = nil
+    group.valid?
+    assert group.errors["name"].include?("can't be blank")
+  end
+  
+  test "the factory works" do
+    group = FactoryGirl.create(:group)
+    assert group.valid?
+  end
+  
 end
