681f6bed60ad2caca8ae09dbd50ff78416ffc37a
Joe Burgess <joemburgess@gmail.com>
2013-05-22 12:06:38 -0400

    Adding VCR Sanitizer and Use Auth Token in Config
     - run it like this
        rake vcr:sanitize[cassette_fil] RAILS_ENV=test

--- test/factories/facebook_auth_factory.rb
+++ test/factories/facebook_auth_factory.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     association :user, factory: :active_user
 
     factory :fb_auth_complete do
-      auth_token "CAACb8HWIoOkBAAZAXDsB2UZAbjHAJnZBrlkrC8OsvNVKZCiU5rgjrcPEiByPcQ4uh4hH8Tn4w3mqjiaqMA2ZBGTLSb9Sym0nfogoyIhqLmvG9Q3fGkfJ1ZCwZC4KoXGO52cdOxWhyinF9DL1nas8IFowkyzU7G50PIZD"
+      auth_token Rails.application.config.vcr_tokens[:facebook][:auth_token]
       expires_at Time.now+5181906
     end
   end
