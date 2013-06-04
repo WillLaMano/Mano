a549c1c963763f259ca2ddc9feabecea01a629f4
Benuuu <benwu00@gmail.com>
2013-06-04 17:54:36 -0400

    Adding 100 years to the google auth factory.
        - Prevents VCR from refetching the oauth token

--- test/factories/google_auth_factory.rb
+++ test/factories/google_auth_factory.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
     factory :google_auth_complete do
       auth_token "ya29.AHES6ZS7kwkYEYtnKukriPzu9MowigcOOzNL1pfyrZuN4DoKgnbRVg"
       refresh_token "1/dEVsWtxoYW7_XY7TX5oCHgZubOGa5ZlV_Oqktqp0RWw"
-      expires_at DateTime.strptime("1369964086","%s")
+      expires_at DateTime.strptime("4115410486","%s")
     end
   end
 end
