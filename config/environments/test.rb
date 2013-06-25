1611b3cfa8b992611ce994880c99ea8d98887c90
Joe Burgess <joemburgess@gmail.com>
2013-06-25 14:29:44 -0400

    Adding Twitter Live Config Variables and Directions

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -65,11 +65,29 @@ Mano::Application.configure do
       :host => ""
     },
 
+    # Directions to get Twitter Auth Token/Secret
+    # This is much more annoying... May be easier to just yank from the DB
+    # 1. Go to https://dev.twitter.com/console
+    # 2. In the "Authentication" dropdown go to "OAuth 1"
+    # 3. Login (if needed) and hit allow
+    # 3. You'll see the page refresh and now you'll have your account in the 
+    #    Authentication dropdown.
+    # 4. Right click anywhere and inspect element.
+    # 5. Refresh the page
+    # 6. Go to the Network tab of the element inspector.
+    # 7. Look for the fethUserCredentials request. Sort by Type..it'll make
+    #    that easier. It's a application/json response.
+    # 8. Click on the fetchUserCredentials item, choose the "Response" tab
+    # 9. Look for the "secret" and "token" keys in the json response
+    # 10. Those are you secret/token
+    
     :twitter => {
       :client_id=>"",
       :client_secret=>"",
       :redirect_uri=>"http://localhost:3000/authorizations/callback/twitter",
-      :host =>""
+      :host =>"",
+      :auth_token=>"",
+      :auth_secret=>""
     },
     # Directions to get Foursquare Auth Token
     # 1. Go to https://developer.foursquare.com/docs/explore
