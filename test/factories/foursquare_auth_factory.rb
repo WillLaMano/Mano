686ec44623ae980174046ac40e8892c73b3c498d
Joe Burgess <joemburgess@gmail.com>
2013-06-25 11:50:50 -0400

    Added in Foursquare Live Testing Factory Item

--- test/factories/foursquare_auth_factory.rb
+++ test/factories/foursquare_auth_factory.rb
@@ -3,7 +3,13 @@ FactoryGirl.define do
     association :user, factory: :active_user
 
     factory :foursquare_auth_complete do
-      auth_token "XSBKF53RKJ3BDNNQ4P1ES4J5O0XJXTYZIZ0NWJRAETYWW4SC"
+      auth_token Rails.application.config.vcr_tokens["foursquare"][:auth_token]
+    end
+
+    if !Rails.application.config.auth["foursquare"][:auth_token].empty?
+      factory :foursquare_auth_complete_live do
+        auth_token Rails.application.config.auth["foursquare"][:auth_token]
+      end
     end
   end
 end
